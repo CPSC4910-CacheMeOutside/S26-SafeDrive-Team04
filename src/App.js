622c32f1afdaaa09778b2a63403319fe5369c93e
Louis Godfrey <lginn26@gmail.com>
2026-02-04 15:38:48 -0500

    Initilized navigation structure Installed routing packages Removed About.js

--- src/App.js
+++ src/App.js
@@ -7,29 +7,64 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
-function FrontPageNav () {
+/* Login component to be implemented 
+function LoginPage() {
+
+}
+*/
+
+
+
+function AboutPage () {
   return (
+    <div>
+      <h1>About our App</h1>
+    </div>
+  );
+}
+
+function HomePage () {
+  return (
+    <div>
+      <h1>Welcome Home</h1>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+
         <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
             <Navbar.Brand href="#home">Safe Drive</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Login</Nav.Link>
-                <Nav.Link href="#link">About</Nav.Link>
+                <Nav.Link href="#home">
+                  <Link to="/">Home</Link>
+                </Nav.Link>
+                <Nav.Link href="#link">
+                  <Link to="/about">About</Link>
+                </Nav.Link>
+                <Nav.Link href="#link">
+                  {/* Login path to be implemented*/}
+                  <Link to="/">Login</Link>
+                </Nav.Link>
             </Nav>
             </Navbar.Collapse>
         </Container>
         </Navbar>
-    );
-}
 
-function App() {
-  return (
-    <div className="App">
-      <FrontPageNav />
+        <Routes>
+          <Route path="/" element={<HomePage />}/>
+          <Route path="/about" element={<AboutPage />}/>
+          {/* login path to be implemented */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
