43f66663324a3bb3d32f8b986c8d08466570c3ea
Louis Godfrey <lginn26@gmail.com>
2026-02-04 14:05:31 -0500

    Replaced React Boilerplate Create a navigation panel for our general public pages

--- src/About.js
+++ src/About.js
@@ -9,23 +9,13 @@ export default function About() {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand href="#home">Safe Drive</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
                 <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                    Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                </NavDropdown.Item>
-                </NavDropdown>
+                <Nav.Link href="#link">Login</Nav.Link>
+                <Nav.Link href="#link">About</Nav.Link>
             </Nav>
             </Navbar.Collapse>
         </Container>
--- src/App.js
+++ src/App.js
@@ -3,26 +3,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button'
 import './App.css';
 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function FrontPageNav () {
+  return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+            <Navbar.Brand href="#home">Safe Drive</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Login</Nav.Link>
+                <Nav.Link href="#link">About</Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
+        </Container>
+        </Navbar>
+    );
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1>It works!</h1>
-        <Button>BootstrapButton</Button>
-        <></>
-      </header>
+      <FrontPageNav />
     </div>
   );
 }
