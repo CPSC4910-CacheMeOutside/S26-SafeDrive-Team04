f58e2c1be758511c445914203dea624a95bb3e06
emilyacunaa <eacuna@clemson.edu>
2026-02-05 11:23:29 -0500

    Connected create password to home page

--- src/App.js
+++ src/App.js
@@ -1,32 +1,50 @@
-import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button'
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import CreatePassword from './create_password';
 
 function App() {
-
-  return <CreatePassword />;
-  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1>It works!</h1>
-        <Button>BootstrapButton</Button>
-        <></>
-      </header>
+      <BrowserRouter>
+
+        <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+            <Navbar.Brand href="#home">Safe Drive</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+                <Nav.Link href="#home">
+                  <Link to="/Home">Home</Link>
+                </Nav.Link>
+                <Nav.Link href="#link">
+                  <Link to="/about">About</Link>
+                </Nav.Link>
+                <Nav.Link href="#link">
+                  <Link to="/profile">Profile</Link>
+                </Nav.Link>
+                <Nav.Link href="#link">
+                  <Link to="/admin">Admin</Link>
+                </Nav.Link>
+                <Nav.Link href="#link">
+                  {/* Login path to be implemented*/}
+                  <Link to="/login">Login</Link>
+                </Nav.Link>
+                <Nav.Link href="#link">
+                  <Link to="/">Create Account</Link>
+                </Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
+        </Container>
+        </Navbar>
+
+        <Routes>
+          <Route path="/" element={<CreatePassword />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
