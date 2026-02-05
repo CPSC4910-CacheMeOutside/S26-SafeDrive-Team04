9400991414934c516aeea05aabf164f07b7073dd
Gabriel <ghilles@clemson.edu>
2026-02-05 11:26:54 -0500

    Preparing to merge

--- src/App.js
+++ src/App.js
@@ -30,26 +30,7 @@ function HomePage () {
   );
 }
 
-function ProfilePage(){
-  const alias = "Breaker_1-9";
-  const points = 144;
-  return(
-    <Container className="py-4" style={{ maxWidth: 700 }}>
-      <h2 classname="mb-3">Driver Profile</h2>
 
-      <div style={{
-        padding: 20,
-        borderRadius: 10,
-        boxShadow: "0 2px 8px rgba(0,0,0,0.1)"
-      }}>
-        <h4>{alias}</h4>
-        <div style={{ fontsize: 14, opacity: 0.7}}>Current Points</div>
-        <div style={{ fontsize: 40, fontweight: 700}}>{points}</div>
-      </div>
-
-    </Container>
-  );
-}
 
 function AdminPage(){
   return(
