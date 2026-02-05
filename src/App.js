34a51fd3dc5600b1b56556cdd1afed627cfc1a81
Gabriel <ghilles@clemson.edu>
2026-02-05 01:00:56 -0500

    Mockup of Admin Point Assign Feature

--- src/App.js
+++ src/App.js
@@ -51,6 +51,24 @@ function ProfilePage(){
   );
 }
 
+function AdminPage(){
+  return(
+    <Container className="py-4" style={{ maxWidth: 800}}>
+      <h2 className="mb-3">Admin Panel</h2>
+
+      <div style={{ padding: 20, borderRedius: 10, boxShadow: "0 2px 8px rgba(0,0,0,0.1)"}}>
+        <h5>Assign Points(mock)</h5>
+        <p>Select a driver to add/subtract points</p>
+        <ul style={{ textAlign: "left"}}>
+          <li>Driver: Breaker_1-9</li>
+          <li>Action: +10 / -10 buttons (next)</li>
+          <li>Reason: "Package Delivered On Time"</li>
+        </ul>
+      </div>
+    </Container>
+  );
+}
+
 function App() {
   return (
     <div className="App">
@@ -72,6 +90,9 @@ function App() {
                   <Link to="/profile">Profile</Link>
                 </Nav.Link>
                 <Nav.Link href="#link">
+                  <Link to="/admin">Admin</Link>
+                </Nav.Link>
+                <Nav.Link href="#link">
                   {/* Login path to be implemented*/}
                   <Link to="/">Login</Link>
                 </Nav.Link>
@@ -84,6 +105,7 @@ function App() {
           <Route path="/" element={<HomePage />}/>
           <Route path="/about" element={<AboutPage />}/>
           <Route path="/profile" element={<ProfilePage />}/>
+          <Route path="/admin" element={<AdminPage />}></Route>
           {/* login path to be implemented */}
         </Routes>
       </BrowserRouter>
