99b3cd31f956b510d5a62113142604311111d9f6
ghoegy1 <ghoegy@clemson.edu>
2026-02-11 14:51:31 -0500

    created a view sponsored drivers button on sponsor dashboard

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ import Sponsor_ViewDrivers from './Sponsor_ViewDrivers';
 import { NavDropdown } from 'react-bootstrap';
 import SponsorPage from './SponsorPage';
 import EditProfilePage from './EditProfilePage';
+import Sponsor_ViewDrivers from './Sponsor_ViewDrivers';
 
 const hideNavs = {
 home: false,
