faee0193ee89e229e54ab4fb658bb0225a47465f
ghoegy1 <ghoegy@clemson.edu>
2026-02-11 17:12:48 -0500

    non-local login redirect works via amplify! testing non-local logout redirect

--- src/LogoutPage.js
+++ src/LogoutPage.js
@@ -8,7 +8,7 @@ function LogoutPage() {
 
   useEffect(() => {
     const clientId = "5qkcg4h6o51nq40der98l7qsvk";
-    const logoutUri = "http://localhost:3000/";
+    const logoutUri = window.location.origin;
     const cognitoDomain = "https://us-east-17kWyOumWk.auth.us-east-1.amazoncognito.com";
 
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
