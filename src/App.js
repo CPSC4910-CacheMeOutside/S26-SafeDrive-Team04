2af20deff76aa1667870191ce4c436264fa11c52
ghoegy1 <ghoegy@clemson.edu>
2026-02-11 16:17:42 -0500

    testing signin routing - non-local

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { BrowserRouter, Routes, Route, Link, useLocation } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link, useLocation, useNavigate } from 'react-router-dom';
 import { useAuth } from 'react-oidc-context';
+import { useEffect } from 'react';
 
 import HomePage from './Home'
 import AboutPage from './About'
@@ -22,6 +23,8 @@ import Sponsor_ViewDrivers from './Sponsor_ViewDrivers';
 
 function App() {
   const auth = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
 
   const hideNavs = {
     home: false,
@@ -32,45 +35,56 @@ function App() {
     creatPass: true
   }
 
+  // when login is successful and the user is a sponsor, they are redirected to the sponsor dashboard
+  useEffect(() => {
+    if (!auth.isAuthenticated) return;
+
+    const groups = auth.user?.profile?.["cognito:groups"];
+
+    if (groups?.includes("Sponsor") && (location.pathname === "/" || location.pathname === "/login")) {
+      navigate("/SponsorPage", { replace: true });
+    }
+  }, [auth.isAuthenticated, location.pathname]);
+
   return (
     <div className="App">
-          <Navbar expand="lg" className="bg-body-tertiary">
-          <Container>
-              <Navbar.Brand href="#home">Safe Drive</Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="#home">Safe Drive</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
                 {!auth.isAuthenticated && <Nav.Link hidden={hideNavs.home} as={Link} to="/">Home</Nav.Link>}
                 {!auth.isAuthenticated && <Nav.Link hidden={hideNavs.about} as={Link} to="/about">About</Nav.Link>}
                 <Nav.Link hidden={hideNavs.profile} as={Link} to="/profile">Profile</Nav.Link>
                 <Nav.Link hidden={hideNavs.admin} as={Link} to="/admin">Admin</Nav.Link>
                 <Nav.Link hidden={hideNavs.creatPass} as={Link} to="/create_password">Create Account</Nav.Link>
                 {auth.isAuthenticated && <Nav.Link as={Link} to="/SponsorPage">My Dashboard</Nav.Link>}
-              </Nav>
-              <Nav className="ms-auto">
+                </Nav>
+                <Nav className="ms-auto">
                 {!auth.isAuthenticated && <Nav.Link as={Link} to="/login">Login</Nav.Link>}
                 {auth.isAuthenticated && <Nav.Link as={Link} to="/edit_profile">Edit Profile</Nav.Link>}
                 {auth.isAuthenticated && <Nav.Link as={Link} to="/logout">Logout</Nav.Link>}
-              </Nav>
-              </Navbar.Collapse>
-          </Container>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
         </Navbar>
- 
+
         <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />}/>
-          <Route path="/about" element={<AboutPage />}/>
-          <Route path="/profile" element={<ProfilePage />}/>
-          <Route path="/admin" element={<AdminPage />}/>
-          <Route path="/create_password" element={<CreatePassword />}/>
-          <Route path="/login" element={<LoginPage />}/>
-          <Route path="/logout" element={<LogoutPage />}/>
-          <Route path="/SponsorPage" element={<SponsorPage />}/>
-          <Route path="/catalog" element={<Catalog />}/>
-          <Route path="/edit_profile" element={<EditProfilePage />}/>
-          <Route path="/sponsor_viewDrivers" element={<Sponsor_ViewDrivers />}/>
-        </Routes>
-      </BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />}/>
+                <Route path="/about" element={<AboutPage />}/>
+                <Route path="/profile" element={<ProfilePage />}/>
+                <Route path="/admin" element={<AdminPage />}/>
+                <Route path="/create_password" element={<CreatePassword />}/>
+                <Route path="/login" element={<LoginPage />}/>
+                <Route path="/logout" element={<LogoutPage />}/>
+                <Route path="/SponsorPage" element={<SponsorPage />}/>
+                <Route path="/catalog" element={<Catalog />}/>
+                <Route path="/edit_profile" element={<EditProfilePage />}/>
+                <Route path="/sponsor_viewDrivers" element={<Sponsor_ViewDrivers />}/>
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
