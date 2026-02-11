c71d73e2ee8292c122a9f0f090b359b90820836b
ghoegy1 <ghoegy@clemson.edu>
2026-02-11 11:41:44 -0500

    added initial logout button to SponsorPage

--- src/LogoutPage.js
+++ src/LogoutPage.js
@@ -4,23 +4,11 @@ import { useAuth } from 'react-oidc-context';
 function LogoutPage() {
   const auth = useAuth();
 
-  auth.removeUser();
-
   useEffect(() => {
-    const cognitoDomain = "https://us-east-17kWyOumWk.auth.us-east-1.amazoncognito.com";
-    const clientId = "5qkcg4h6o51nq40der98l7qsvk";
-    const logoutUri = "https://sponsor-profile-page.d2jawpaet8g6c9.amplifyapp.com/";
-
-    localStorage.clear();
-    sessionStorage.clear();
-
-    if (window.location.hostname.includes("amplifyapp.com")) {
-      logoutUri = window.location.href.split("/")[0] + "//" + window.location.hostname;
+    if (auth.isAuthenticated && !auth.isLoading) {
+        auth.signoutRedirect({ state: { postLogoutRedirectUri: '/' } });
     }
-
-    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
-
-  }, []);
+  }, [auth]);
 
   return null;
 }
