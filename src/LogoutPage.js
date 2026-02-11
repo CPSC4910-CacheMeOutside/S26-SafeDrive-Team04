3b21289a27bd8d074849a1ca23885affcf10e49a
ghoegy1 <ghoegy@clemson.edu>
2026-02-11 12:46:46 -0500

    SponsorPage logout button signs user out and redirects to home page

--- src/LogoutPage.js
+++ src/LogoutPage.js
@@ -4,11 +4,16 @@ import { useAuth } from 'react-oidc-context';
 function LogoutPage() {
   const auth = useAuth();
 
+  auth.removeUser();
+
   useEffect(() => {
-    if (auth.isAuthenticated && !auth.isLoading) {
-        auth.signoutRedirect({ state: { postLogoutRedirectUri: '/' } });
-    }
-  }, [auth]);
+    const clientId = "5qkcg4h6o51nq40der98l7qsvk";
+    const logoutUri = "http://localhost:3000/";
+    const cognitoDomain = "https://us-east-17kWyOumWk.auth.us-east-1.amazoncognito.com";
+
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+
+  }, []);
 
   return null;
 }
