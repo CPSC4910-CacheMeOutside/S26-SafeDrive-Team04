521783b9471e8e54b3289f59f5292f3de1c9c731
ghoegy1 <ghoegy@clemson.edu>
2026-02-11 17:26:33 -0500

    remote logout test

--- src/LogoutPage.js
+++ src/LogoutPage.js
@@ -8,8 +8,12 @@ function LogoutPage() {
 
   useEffect(() => {
     const clientId = "5qkcg4h6o51nq40der98l7qsvk";
-    const logoutUri = window.location.origin;
     const cognitoDomain = "https://us-east-17kWyOumWk.auth.us-east-1.amazoncognito.com";
+
+    let logoutUri = "http://localhost:3000";
+    if (window.location.hostname.includes("amplifyapp.com")) {
+      logoutUri = window.location.href.split("/")[0] + "//" + window.location.hostname;
+    }
 
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
 
