80bcf6f9e1cf13a12b4babb40c376121aa99ecf3
Louis Godfrey <lginn26@gmail.com>
2026-02-12 17:54:20 -0500

    Removed redundant import cause yet another build failure

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ import LogoutPage from './LogoutPage';
 import React, { useState } from 'react';
 import Catalog from './Catalog';
 import LogoutPage from './LogoutPage';
-import SponsorPage from './SponsorPage';
 import EditProfilePage from './EditProfilePage';
 import Sponsor_ViewDrivers from './Sponsor_ViewDrivers';
 import SponsorSettingsPage from './SponsorSettingsPage';
