063a4e559c05089ced4646f3b4adfbaf0d51605b
mboyden-lab <mboyden@g.clemson.edu>
2026-02-12 15:43:14 -0500

    reverting my changes that were supposed to go to my branch not dev

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AuthProvider } from 'react-oidc-context';
-import { ConversionRatioProvider } from './ConversionRatioContext';
-import { NotificationProvider } from './NotificationContext';
-import { PointsProvider } from './PointsContext';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 
@@ -34,13 +31,7 @@ root.render(
       </AuthProvider>
     </BrowserRouter>
     <AuthProvider {...cognitoAuthConfig}>
-      <ConversionRatioProvider>
-        <NotificationProvider>
-          <PointsProvider>
-            <App />
-          </PointsProvider>
-        </NotificationProvider>
-      </ConversionRatioProvider>
+        <App />
     </AuthProvider>
   </React.StrictMode>
 );
