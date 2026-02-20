77acf38f7c690ba75ef55c5e372bc174fce8aec5
Louis Godfrey <lginn26@gmail.com>
2026-02-20 09:39:03 -0500

    Removed duplicate sandbox files

--- src/App.jsx
+++ src/App.jsx
@@ -30,9 +30,14 @@ import DriverNotificationsPage from './DriverNotificationsPage';
  
 import UpdateAbout from './UpdateAbout';
 
+/* Add the backend */
+import { Amplify } from 'aws-amplify';
+import outputs from '../amplify_outputs.json';
+
+Amplify.configure(outputs);
+
 
 function App() {
-
   /* Nav config */
   const auth = useAuth();
   const navigate = useNavigate();
