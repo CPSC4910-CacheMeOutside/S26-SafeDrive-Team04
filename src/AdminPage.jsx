ec40f2913e22ac31d2b87c4c19d2f6c028d9a48b
Gabriel <ghilles@clemson.edu>
2026-02-24 15:41:58 -0500

    port over changes for sponsor page

--- src/AdminPage.jsx
+++ src/AdminPage.jsx
@@ -1,6 +1,57 @@
 import { Container, Row, Col, Stack, Card } from 'react-bootstrap';
-
+import { useAuth } from 'react-oidc-context';
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Form from "react-bootstrap/Form"
+import Tabs from "react-bootstrap/Tabs"
+import Tab from"react-bootstrap/Tab"
 export default function AdminPage(){
+  
+  const [drivers, setDrivers] = useState([
+    { id: 1, name: "Bo Darvilel", points: 200 },
+    { id: 2, name: "Cledus Snow", points: 156 },
+    { id: 3, name: "Hot-Pants Hillard", points: 186 },
+    { id: 4, name: "Burt Reynolds", points: 330 },
+    { id: 5, name: "Jerry Reed", points: 300 }
+  ]);
+
+  const [selectedId, setSelectedId] = useState(1);
+  const [amount, setAmount] = useState(10);
+  const [sortMode, setSortMode] = useState("id");
+  const [description, setDescription] = useState("");
+  const [logs, setLogs] = useState([]);
+  const pointAdjust = (value) => {
+    const timestamp = new Date().toLocaleString();
+    setDrivers(prev =>
+      prev.map(d =>
+        d.id === selectedId ? { ...d, points: d.points + value } : d
+      )
+    );
+    setLogs(prev => [
+      {
+        driver: selectedDriver.name,
+        change: value,
+        reason: description || "No Reason Provided",
+        time: timestamp
+      },
+      ...prev
+    ]);
+    setDescription("");
+  };
+  const sortedDrivers = [...drivers].sort((a, b) => {
+    if (sortMode === "points") return b.points - a.points;
+    if (sortMode === "id") return a.id - b.id;
+    return 0;
+  });
+    
+    const navigate = useNavigate();
+
   return(
     <Container className="mt-4">
       <div style={{ position: "relative", minHeight: "100vh", padding: "40px" }}>
