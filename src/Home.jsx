cf5f1410fe9cb9da771b26baa00f73dfab155606
Louis Godfrey <lginn26@gmail.com>
2026-03-03 11:04:49 -0500

    Updated home page to direct users to the sponsor listing

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,12 @@
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function HomePage () {
   return (
     <div style={{ position: "relative", minHeight: "100vh", padding: "60px" }}> 
       <h1><strong>Welcome to Safe Drive!</strong></h1>
+      <p>Ready to drive safely. Apply to a sponsor today!</p>
+      <Button as={Link} to='/sponsor-list'>Apply Now!</Button>
     </div>
   );
 }
