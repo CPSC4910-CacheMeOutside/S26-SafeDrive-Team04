4af70820699257856c6f02a5c695f589da610dc5
Louis Godfrey <lginn26@gmail.com>
2026-03-10 16:13:42 -0400

    Created a library to interact with the store API

---
+++ src/catalog/ store-api.jsx
@@ -0,0 +1,56 @@
+import { title } from "node:process";
+
+export class PalatziQueryStruct {
+
+    constructor(title='', limit=10, offset=0, minPrice=0, maxPrice=Number.MAX_SAFE_INTEGER, category='') {
+        this.title = title;
+        this.limit = limit;
+        this.offset = offset;
+        this.price_min = minPrice;
+        this.price_max = maxPrice,
+        this.categorySlug = category
+    }
+}
+
+function makeApiString(filters) {
+    let apiString = 'https://api.escuelajs.co/api/v1/products'
+
+    Object.keys(filters).filter(f => filters[f] != null)
+    .forEach((filter, index) => {
+
+        if (index === 0 ) {
+            apiString += '/?'
+        }
+
+        apiString += `${filter}=${filters[filter]}`
+
+        if (!(index === keys.length-1)) {
+            apiString += '&'
+        }
+    });
+}
+
+export async function getProducts(filters){
+    try {
+        const apiStr = makeApiString(filters)
+        const storeRequest = await fetch(apiStr);
+        const storeData = await storeRequest.json();
+
+        let products = storeData.map(rawProduct => (
+        {
+            pId: rawProduct.id,
+            title: rawProduct.title,
+            imgs: rawProduct.images,
+            synop: rawProduct.slug,
+            desc: rawProduct.description,
+            catagory: rawProduct.category.name,
+            price: rawProduct.price,
+            available: true,
+        }
+        ));
+        return products;
+    } catch (err) {
+        console.log("Storefront product retrieval failed: ", err);
+        return null;
+    }
+}
