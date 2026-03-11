585ed7a8daec260ef8574da0cef39a5606ccab56
Louis Godfrey <lginn26@gmail.com>
2026-03-11 18:32:47 -0400

    Merged auth changes

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,16 +1,17 @@
 import { defineAuth } from '@aws-amplify/backend';
 
+ 
 export const auth = defineAuth({
   loginWith: {
     email: true,
     externalProviders: {
       callbackUrls: [
         'http://localhost:5173/callback',
-        'https://driverlogin.d2jawpaet8g6c9.amplifyapp.com/callback',
+        'https://dev.d2jawpaet8g6c9.amplifyapp.com/callback',
       ],
       logoutUrls: [
         'http://localhost:5173/',
-        'https://driverlogin.d2jawpaet8g6c9.amplifyapp.com/',
+        'https://dev.d2jawpaet8g6c9.amplifyapp.com/',
       ],
     },
   },
