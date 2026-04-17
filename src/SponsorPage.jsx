4a0ea8f4d0588497c12e1f95bb797d5e0576d66b
ghoegy1 <ghoegy@clemson.edu>
2026-04-17 13:27:40 -0400

    init

--- src/SponsorPage.jsx
+++ src/SponsorPage.jsx
@@ -10,7 +10,6 @@ import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
-import useAmplifyAuth from './UseAmplifyAuth';
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
