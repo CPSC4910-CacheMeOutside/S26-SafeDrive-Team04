307c2f0063f115e424927d5d147473e10c171596
Louis Godfrey <lginn26@gmail.com>
2026-04-18 10:05:03 -0400

    Completed populateCatalogs Removed unused imports from resource

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,6 +1,4 @@
 import { a, defineData, type ClientSchema} from '@aws-amplify/backend';
-import { Product } from 'aws-cdk-lib/aws-servicecatalog';
-import { id } from 'aws-sdk/clients/datapipeline';
 import { updateStorefront } from '../functions/updateStorefront/resource';
  
 // Create our About Table
